8820497d0ffa502c778c1e8fd5676f1f776d16fe
Levug <92178666+Levug@users.noreply.github.com>
2021-11-21 22:06:38 +0300

    Create matrix.c

---
+++ программирование/matrix.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+int main()
+{
+    int R[3][3], B[3], i, j, min=666, diap=187;
+    for (i=0; i<3; i++) // задаю матрицу  R
+    {
+        for (j=0; j<3; j++)
+        {
+            R[i][j]=rand()%diap;
+        }
+    }
+for (i=0; i<3; i++)
+{
+    for(j=0; j<3; j++)
+    {
+      printf("%d ", R[i][j]);
+    }
+    printf("\n");
+}   
+    for (j=0, i=0; i<3; j++)// задаю массив B крайне извращенным методом
+    {
+        if (R[i][j]<min)
+        {
+          min=R[i][j];      
+        }        
+        if (j==2)
+        {
+            B[i]=min;
+            i++;
+            j=0;
+            min=666;
+        }     
+    }
+    for (i=0; i<3; i++)//вывожу массив В
+    {
+        printf(" B=%i", B[i]);
+    }
+    return 0;
+}
