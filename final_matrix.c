bd7e379dde1428aa24bb218b1c387864bd146ed4
Levug <92178666+Levug@users.noreply.github.com>
2021-11-21 19:23:17 +0300

    Update final_matrix.c

--- final_matrix.c
+++ final_matrix.c
@@ -3,7 +3,7 @@
 #include <conio.h>
 int main()
 {
-    int R[2][2], B[2], i, j, min, diap=9;
+    int R[3][3], B[3], i, j, min, diap=9;
     for (i=0; i<3; i++) // задаю матрицу  R
     {
         for (j=0; j<3; j++)
