5d93f48a34945704014907dc17e000b56361e0f8
Levug <92178666+Levug@users.noreply.github.com>
2022-01-19 19:10:25 +0300

    доработка

--- программирование/1 задание пробник.c
+++ программирование/1 задание пробник.c
@@ -2,32 +2,29 @@
 
 #include <stdio.h>
 #include <math.h>
-fac(int num)
+__int64 fac(int num)
 {
-    int k = 1, i;
-    for (i = 1; num > 1 ; num--)
+    int  i;
+    __int64 k = 1;
+    for (i = 1; num >= i ; i++)
     {
-        k = num*k;
+        k = i*k;
     }
     return k;
 }
 int main()
 {
     int n = 1, S1;
-    float x = 1, y = 1,y1,c = 1, S = 0, e = pow(10, -4);
-    /*
+    float x = 1, y1,c = 1, e = pow(10, -4);
+    double y = 0.5, S = 0;
     while (fabs(y) > e)
     {
-        //y = pow(-1, n + 1)*(pow((5+0.2*x), 2*n - 1)/(fac(2*n)));
         y = pow(-1, (n+1))*pow(5+0.2*x, 2*(n-1))/(fac(2*n));
-        S = S + y;printf("n=%i c = %f - %f = %f \n", n, y, y1, c);
+        S = S + y;
+        printf("%i) y = %f\n", n, y);
         n++;
         
     }
-    */
-
-   S1 = fac(14);
-    printf("S = %i", S1);
-
+    printf("S = %f", S);
     return 0;
 }
